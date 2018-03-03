202f5cfaf20ac7dd58c7558e57b4c258e0ce6152
Robert W <robwash@gmail.com>
2018-03-03 04:03:05 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -10,7 +10,11 @@ class Post
     end
 
     def author_name
-      self.author.name
+      if self.author
+        self.author.name
+      else
+        nil
+      end
     end
 
     #def some_post
