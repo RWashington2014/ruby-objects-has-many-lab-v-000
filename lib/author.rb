950c9899bc115b5e742a514de7e0e5e1d097c380
Robert W <robwash@gmail.com>
2018-03-03 03:24:17 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -18,10 +18,9 @@ class Author
   end
 
   def add_post_by_title(post)
-    post = Post.new(post)
-    @posts << post
-    post.title = post.name
-    post.author = self
+     item = Post.new(post)
+    @posts << item
+    
     @@post_count += 1
   end
 
